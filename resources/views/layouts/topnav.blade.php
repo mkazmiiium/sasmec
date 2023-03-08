8a4860523b3f324a093b1231cef70e8d43b61033
abuyusif01 <abubakaryusuf951@gmail.com>
2023-03-08 14:23:36 +0800

    sasmec: fix button color
    Signed-off-by: abuyusif01 <abubakaryusuf951@gmail.com>

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -1,30 +1,32 @@
 <!-- ########## START: HEAD PANEL ########## -->
 <div class="sl-header">
-    <div class="sl-header-left">
-      
-    </div><!-- sl-header-left -->
-    <div class="sl-header-right">
-      <nav class="nav">
-        <div class="dropdown">
-          <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
-            <span class="logged-name">{{ Auth::user()->name }}</span></span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-header wd-200">
-            <ul class="list-unstyled user-profile-nav">
-              <li>{{ Auth::user()->dept }}</li>
-              <form method="POST" action="{{ route('logout') }}">
-                  @csrf
-                  <li><a href="{{ route('logout') }}" 
-                            onclick="event.preventDefault();
+  <div class="sl-header-left">
+
+  </div><!-- sl-header-left -->
+  <div class="sl-header-right">
+    <nav class="nav">
+      <div class="dropdown">
+        <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+          <span class="logged-name">{{ Auth::user()->name }}</span></span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-header wd-200">
+          <ul class="list-unstyled user-profile-nav">
+            <li>{{ Auth::user()->dept }}</li>
+            <form method="POST" action="{{ route('logout') }}">
+              @csrf
+              <li style="background-color: green;"><a href="{{ route('logout') }}" onclick="event.preventDefault();
                             this.closest('form').submit();">
-                      <i class="icon ion-power"></i> Sign Out</a>
-              </form>
-              </li>
-            </ul>
-          </div><!-- dropdown-menu -->
-        </div><!-- dropdown -->
-      </nav>
-     
-    </div><!-- sl-header-right -->
-  </div><!-- sl-header -->
-  <!-- ########## END: HEAD PANEL ########## -->
+                  <i class="icon ion-power"></i> Sign Out</a>
+
+              <li style="background-color: blue; "><a href="{{ route('register') }}" >
+                  <i class="icon ion-register"></i> Register New User</a>
+            </form>
+            </li>
+          </ul>
+        </div><!-- dropdown-menu -->
+      </div><!-- dropdown -->
+    </nav>
+
+  </div><!-- sl-header-right -->
+</div><!-- sl-header -->
+<!-- ########## END: HEAD PANEL ########## -->
