73f9080fa4412fb2b19f126d4bec873d03a29b93
abuyusif01 <abubakaryusuf951@gmail.com>
2023-07-20 14:33:38 +0800

    sascoms: show ndsc role a dashboard
    Signed-off-by: abuyusif01 <abubakaryusuf951@gmail.com>

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        if (Auth::user()->role == 'dsc') {
+        if (Auth::user()->role == 'dsc' || Auth::user()->role == 'ndsc') {
             return view('User.dsc_index');
         } else if (Auth::user()->role == 'admin') {
             return view('User.admin_index');
